c6814417e5b929ab7b4e81330bcc5a50610c3adf
Christian Tortorella <Christiand.Tortorellar@gmail.com>
2021-08-26 17:39:46 -0300

    5. Creation <Road To Complete - 4°>
    Add the game menu, options, canvas and animations. Also I create some folders to manage better the project.

---
+++ Assets/2. Scripts/1. Scripts_Menu/Logic_MainMenu.cs
@@ -0,0 +1,21 @@
+﻿// 1° MainMenu Script: Is in charge of the actions of the buttons. 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Logic_MainMenu : MonoBehaviour
+{
+    #region Methods to use:
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("MainLevel");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Closing Game...");
+        Application.Quit();
+    }
+    #endregion
+}
