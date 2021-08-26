2ed9d4c6db2c2a91daa9b4d6fdd0194ab2919780
Christian Tortorella <Christiand.Tortorellar@gmail.com>
2021-08-26 19:51:23 -0300

    6. Creation <Road To Complete - 5°>
    Add the loading bar panel. And build the body of the script.

--- Assets/2. Scripts/1. Scripts_Menu/Logic_MainMenu.cs
+++ Assets/2. Scripts/1. Scripts_Menu/Logic_MainMenu.cs
@@ -1,16 +1,36 @@
 ﻿// 1° MainMenu Script: Is in charge of the actions of the buttons. 
 
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Logic_MainMenu : MonoBehaviour
 {
+    #region Variables to use:
+    public GameObject loadingScreen;
+    public Slider slider;
+    #endregion
+
     #region Methods to use:
-    public void PlayGame()
+    public void PlayGame(string sceneName)
     {
-        SceneManager.LoadScene("MainLevel");
+        StartCoroutine(LoadAsynchronously(sceneName));
+    }
+    private IEnumerator LoadAsynchronously (string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+
+            yield return null;
+        }
     }
     public void QuitGame()
     {
