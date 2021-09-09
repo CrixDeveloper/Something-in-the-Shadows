dad676f3bcb72db9333d82c152dbc901c5592866
Christian Tortorella <Christiand.Tortorellar@gmail.com>
2021-09-09 10:28:58 -0300

    17. <Last Details - Game Completed>.
    Updates.

---
+++ Assets/2. Scripts/2. Scripts_Level/RandomTurnOff.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTurnOff : MonoBehaviour
+{
+    #region Method:
+    public GameObject flashLightOff;
+     
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (CountDown_Logic.secondsLeft == Random.Range(0, 180))
+        {
+            flashLightOff.SetActive(false);
+        }
+    }
+    #endregion
+}
